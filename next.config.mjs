d3efc4abaf5a3e5626d93c97234c74f763b07f61
amaan-exe <amanullah2607main@gmail.com>
2025-12-22 02:00:41 +0530

    Configure allowed image domains in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,18 @@
 const nextConfig = {
     images: {
         remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-                pathname: '/**',
-            },
+            { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+            { protocol: 'https', hostname: 'images.unsplash.com' },
+            { protocol: 'https', hostname: 'images-pw.pixieset.com' },
+            { protocol: 'https', hostname: 'lookaside.instagram.com' },
+            { protocol: 'https', hostname: 'www.marriagecolours.com' },
+            { protocol: 'https', hostname: 'weddingsutra.com' },
+            { protocol: 'https', hostname: 'content.jdmagicbox.com' },
+            { protocol: 'https', hostname: 'vdiexports.com' },
+            { protocol: 'https', hostname: 'divinecaterers.in' },
+            { protocol: 'https', hostname: 'lookaside.fbsbx.com' },
+            { protocol: 'https', hostname: 'img.weddingbazaar.com' },
+            { protocol: 'https', hostname: 'media.weddingz.in' },
         ],
     },
 };
