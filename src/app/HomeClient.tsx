c2dc6880403f10e134a71cb2e5582dbb89447aa1
amaan-exe <amanullah2607main@gmail.com>
2026-01-07 23:57:13 +0530

    chore: Remove Contact section from home page

--- src/app/HomeClient.tsx
+++ src/app/HomeClient.tsx
@@ -48,7 +48,6 @@ export default function HomeClient() {
                 <Testimonials />
                 <GoogleReviews />
                 <FAQ />
-                <Contact />
                 <CTA />
                 <Footer />
                 <WhatsAppButton />
